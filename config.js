4ca3f94d9faaf1eda805e4cac73f651ba07f13bb
Matthew Lepley <mdlepley@gmail.com>
2017-08-07 21:28:12 -0500

    config updates

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ var config = {
 			defaultUri: "mongodb://mdlepley:PhaseShift!00@cluster0-shard-00-00-mtarl.mongodb.net:27017,cluster0-shard-00-01-mtarl.mongodb.net:27017,cluster0-shard-00-02-mtarl.mongodb.net:27017/mongopop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 		},
 		mockarooUrl: "http://www.mockaroo.com/536ecbc0/download?count=1000&key=48da1ee0"
+		//https://api.mockaroo.com/api/a9df5280?count=1000&key=a9c5e9f0
 	},
 	makerMongoDBURI: "mongodb://localhost:27017/maker?authSource=admin",
 	checkinCollection: "foursq"
