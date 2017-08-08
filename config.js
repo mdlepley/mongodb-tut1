d4086c2acd1a2d97b0318197bbe3e0955b51789c
Matthew Lepley <mdlepley@gmail.com>
2017-08-07 21:27:56 -0500

    db updates

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
 		mongodb: {
 			defaultDatabase: "mongopop",
 			defaultCollection: "simples",
-			defaultUri: "mongodb://localhost:27017"
+			defaultUri: "mongodb://mdlepley:PhaseShift!00@cluster0-shard-00-00-mtarl.mongodb.net:27017,cluster0-shard-00-01-mtarl.mongodb.net:27017,cluster0-shard-00-02-mtarl.mongodb.net:27017/mongopop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 		},
 		mockarooUrl: "http://www.mockaroo.com/536ecbc0/download?count=1000&key=48da1ee0"
 	},
